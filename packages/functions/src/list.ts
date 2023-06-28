cf1e0040d0657744d14903b8eab2899eef9d87c7
Telvin Mathews <tmathews@appddictionstudio.com>
2023-06-28 14:34:50 -0500

    added list lamda function

---
+++ packages/functions/src/list.ts
@@ -0,0 +1,21 @@
+import {Table} from 'sst/node/table';
+import handler from '../../core/src/handler';
+import dynamodb from '../../core/src/dynamodb';
+
+export const main = handler(async () => {
+  const params = {
+    TableName: Table.Notes.tableName,
+    // 'KeyConditionExpression' defines the condition for the query
+    // - 'userId = :userId': only return items with matching 'userId'
+    // partition key
+    KeyConditionExpression: 'userId = :userId',
+    // 'ExpressionAttributeValues' defines the value in the condition
+    // - ':userId': defines 'userId' to be the id of the author
+    ExpressionAttributeValues: {
+      ':userId': '123'
+    }
+  };
+  const result = await dynamodb.query(params);
+  // Return the matching list of items in response body
+  return result.Items;
+});
