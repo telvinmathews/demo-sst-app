306165ddf50bfd7f528f5c1c2eaed1e949167867
Telvin Mathews <tmathews@appddictionstudio.com>
2023-06-28 14:34:22 -0500

    updated imports

--- packages/functions/src/get.ts
+++ packages/functions/src/get.ts
@@ -1,18 +1,20 @@
 import {Table} from 'sst/node/table';
-import handler from '../../core/handler';
-import dynamodb from '../../core/dynamodb';
+import handler from '../../core/src/handler';
+import dynamodb from '../../core/src/dynamodb';
 
 export const main = handler(async (event: any) => {
   const params = {
     TableName: Table.Notes.tableName,
-    key: {
-      userId: '123',
-      noteId: event.pathParameters.id
+    // 'Key' defines the partition key and sort key of the item to be retrieved
+    Key: {
+      userId: '123', // The id of the author
+      noteId: event.pathParameters.id // The id of the note from the path
     }
   };
   const result = await dynamodb.get(params);
   if (!result.Item) {
     throw new Error('Item not found.');
   }
+  // Return the retrieved item
   return result.Item;
 });
